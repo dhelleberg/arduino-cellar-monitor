7a5a3bf0c8f6efd12df0d5cb2a4e4b8281e9f527
Dominik Helleberg <dominik.helleberg@inovex.de>
2016-11-12 13:52:41 +0100

    added LDR sensor

---
+++ src/DataLogger.cpp
@@ -0,0 +1,47 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT22 //DHT11, DHT21, DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+int ldr1_value = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println("Setup -> done");
+}
+
+void readDHT()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(t) || isnan(h))
+  {
+    Serial.println("DHT22 konnte nicht ausgelesen werden");
+  }
+  else
+  {
+    Serial.print("Luftfeuchte: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperatur: ");
+    Serial.print(t);
+    Serial.println(" C");
+  }
+}
+
+void readLDR() {
+  ldr1_value = analogRead(0);
+  Serial.print("LDR Value:");
+  Serial.println(ldr1_value);
+}
+
+void loop()
+{
+  delay(1000);
+  readDHT();
+  readLDR();
+}
